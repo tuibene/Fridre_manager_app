fcadaa7f61a85a329cbc78927244f4b0ca7e951e
TrungKa <125136900+trungka982004@users.noreply.github.com>
2025-04-11 11:27:58 +0700

    Minor changes

--- ios/test/widget_test.dart
+++ ios/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ios/main.dart';
+import 'package:ntb/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
